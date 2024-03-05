abf23e2961226aaef98e8fcbfc2cdb564c4c2ad1
Wangxinyu <2397303486@qq.com>
2024-03-05 09:27:22 +0800

    创建项目

---
+++ src/main/java/com/yu/service/MytestGithubShare2.java
@@ -0,0 +1,21 @@
+/*******************************************************************************
+ * Package: com.yu.service
+ * Type:    MytestGithubShare2
+ * Date:    2024/3/5 9:26
+ *
+ * Copyright (c) 2024 HUANENG GUICHENG TRUST CORP.,LTD All Rights Reserved.
+ *
+ * You may not use this file except in compliance with the License.
+ *******************************************************************************/
+package com.yu.service;
+
+/**
+ *
+ * @author Wangxinyu
+ * @date 2024/3/5 9:26
+ */
+public class MytestGithubShare2 {
+    public static void main(String[] args) {
+        System.out.println("你好欣宇");
+    }
+}
